017391740758f9ebb9fc3a49de4406019a0441bd
Laetitia Langlois <laetitia.langlois@cosmian.com>
2023-10-04 09:59:58 +0200

    update overview page text

--- src/Overview.tsx
+++ src/Overview.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Image, Link, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react";
+import { Flex, Heading, Image, Link, Stack, Text } from "@chakra-ui/react";
 import ArchitectureSchema from "./assets/architecture_schema.svg";
 
 type OverviewProps = {
@@ -12,19 +12,14 @@ const Overview: React.FC<OverviewProps> = ({ onTabChange }) => {
           Overview
         </Heading>
         <Text>
-          This interface aims to show how Cosmian’s solutions can be implemented to add security on your current workflows - using{" "}
-          <b>client-side encryption</b>:
+          This interface demonstrates how Cosmian's solutions can be implemented to add <b>client-side encryption</b> to your current
+          workflows.
         </Text>
-        <UnorderedList>
-          <ListItem>architecture of implementations</ListItem>
-          <ListItem>code blocks to easily use Cosmian technologies</ListItem>
-        </UnorderedList>
-        <Text>
-          With client-side encryption, content is encrypted in the customer's browser before the data is transmitted or stored on cloud or
-          application services, using keys provided directly by the customer. This approach significantly minimizes the attack surface, as
-          the application and data layers within the zero-trust environment process only encrypted data and have no access to decryption
-          keys.{" "}
-        </Text>
+
+        <Heading as="h3" size="md" mt={4}>
+          Architecture of implementations
+        </Heading>
+        <Text>Cosmian provides code blocks that make using its technologies to implement client-side encryption easy.</Text>
         <Image
           boxSize="100%"
           maxWidth={900}
@@ -35,32 +30,50 @@ const Overview: React.FC<OverviewProps> = ({ onTabChange }) => {
           my={6}
         />
 
-        <UnorderedList spacing={3}>
-          <ListItem>
-            To further enhance the security provided by application-level encryption, it is crucial to employ{" "}
-            <b>a robust encryption scheme</b> like <em>Covercrypt</em>. <br />
-            <em>Covercrypt</em> mitigates the risks associated with key leakage from the presentation layer and addresses potential security
-            risks such as rights escalation attacks and authorization misconfigurations. <br />
-            <Link color="orange.500" onClick={() => onTabChange(1)}>
-              → checkout attribute-based encryption example
-            </Link>
-          </ListItem>
-          <ListItem>
-            Using our Key Management Service (<em>Cosmian KMS</em>) and its PKI, you can <b>share safely keys between users</b> <br />
-            <Link color="orange.500" onClick={() => onTabChange(2)}>
-              → checkout Key distribution example
-            </Link>
-          </ListItem>
-          <ListItem>
-            One of the first negative consequences of using application-level encryption is the inability of the storage layer to search for
-            data, and most applications rely on search features for data extraction. This is because the search engine cannot decrypt the
-            data and, therefore, cannot index it. To solve this issue, Cosmian provides <em>Findex</em>, a{" "}
-            <b>Searchable Encryption scheme that allows the building of encrypted indexes</b>.<br />
-            <Link color="orange.500" onClick={() => onTabChange(3)}>
-              → checkout Search on encrypted data example
-            </Link>
-          </ListItem>
-        </UnorderedList>
+        <Heading as="h3" size="md" mt={4}>
+          Client-side encryption
+        </Heading>
+        <Text>
+          With client-side encryption, content is encrypted in the customer's browser or connector before it is transmitted to the cloud
+          application servers. The customer manages the encryption keys. This approach significantly reduces the attack surface, as the
+          application and data layers within the zero-trust environment process only encrypted data and have no clear text access to the
+          decryption keys.
+        </Text>
+
+        <Heading as="h3" size="md" mt={4}>
+          State-of-the-art post-quantum encryption with embedded access policies
+        </Heading>
+        <Text>
+          To further enhance the security provided by application-level encryption, employing a robust encryption scheme like Covercrypt is
+          crucial. Covercrypt mitigates the risks associated with key leakage from the presentation layer and addresses potential security
+          risks such as rights escalation attacks and authorization misconfigurations. <br />
+          <Link color="orange.500" onClick={() => onTabChange(1)}>
+            → attribute-based encryption example
+          </Link>
+        </Text>
+
+        <Heading as="h3" size="md" mt={4}>
+          Key distribution
+        </Heading>
+        <Text>
+          Using Cosmian's Key Management Service (Cosmian KMS) and PKI, users can safely share their keys via the zero trust layer. <br />
+          <Link color="orange.500" onClick={() => onTabChange(2)}>
+            → key distribution with Cosmian KMS and PKI example
+          </Link>
+        </Text>
+
+        <Heading as="h3" size="md" mt={4}>
+          Search on encrypted data
+        </Heading>
+        <Text>
+          One of the drawbacks of using application-level encryption is that the storage layer cannot search for data, and most applications
+          rely on search features for data extraction. This is because the search engine cannot decrypt the data and, therefore, cannot
+          index it. To solve this issue, Cosmian provides Findex, a searchable encryption scheme that allows the building of encrypted
+          indexes. <br />
+          <Link color="orange.500" onClick={() => onTabChange(3)}>
+            → searchable encryption scheme example
+          </Link>
+        </Text>
       </Stack>
     </Flex>
   );
